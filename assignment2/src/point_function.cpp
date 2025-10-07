8fa82b8ce9151d0f8d26e78e65006e4a99130e84
anniehuang <annie.huang921002@gmail.com>
2025-10-07 19:30:15 +0800

    add get_quadrant, collinear, and modify distance function

--- assignment2/src/point_function.cpp
+++ assignment2/src/point_function.cpp
@@ -7,12 +7,31 @@ double get_euclidean_distance(Point p, Point q)
 {
     double dx = q.x - p.x;
     double dy = q.y - p.y;
-    return std::sqrt(dx * dx + dy * dy);
+    return std::abs(std::sqrt(dx * dx + dy * dy));
 }
 
 bool is_collinear(Point p, Point q, Point r)
 {
-    return (q.y - p.y) * (r.x - q.x) == (r.y - q.y) * (q.x - p.x);
+    // Cross product will be zero if any two vectors are collinear
+    double cross_product = (q.x - p.x) * (r.y - p.y) - (q.y - p.y) * (r.x - p.x);
+    return std::abs(cross_product) < 1e-9;
+}
+std::string get_quadrant(Point p)
+{
+    if (p.x > 0 && p.y > 0)
+        return "Quadrant 1";
+    else if (p.x < 0 && p.y > 0)
+        return "Quadrant 2";
+    else if (p.x < 0 && p.y < 0)
+        return "Quadrant 3";
+    else if (p.x > 0 && p.y < 0)
+        return "Quadrant 4";
+    else if (p.x == 0 && p.y != 0)
+        return "On Y axis";
+    else if (p.y == 0 && p.x != 0)
+        return "On X axis";
+    else
+        return "Origin";
 }
 Point get_midpoint(Point p, Point q)
 {
