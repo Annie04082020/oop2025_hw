5383b7a917921289384a56906b20184d5b59f29c
anniehuang <annie.huang921002@gmail.com>
2025-10-11 00:10:04 +0800

    update error value

--- assignment3/src/triangle.cpp
+++ assignment3/src/triangle.cpp
@@ -20,9 +20,9 @@ double Triangle::perimeter() const
 bool Triangle::is_equilateral() const
 {
     auto [a, b, c] = get_sides(*this);
-    bool a_eq_b = std::abs(a - b) < 1e-9;
-    bool b_eq_c = std::abs(b - c) < 1e-9;
-    bool a_eq_c = std::abs(a - c) < 1e-9;
+    bool a_eq_b = std::abs(a - b) < 1e-12;
+    bool b_eq_c = std::abs(b - c) < 1e-12;
+    bool a_eq_c = std::abs(a - c) < 1e-12;
     return a_eq_b && b_eq_c && a_eq_c;
 }
 
@@ -32,5 +32,5 @@ bool Triangle::is_right() const
     double max_side_sqr = std::max({a * a, b * b, c * c});
     double sum_sqr = a * a + b * b + c * c;
     double other_sum_sqr = sum_sqr - max_side_sqr;
-    return std::abs(other_sum_sqr - max_side_sqr) < 1e-9;
+    return std::abs(other_sum_sqr - max_side_sqr) < 1e-12;
 }
