486387c29a817118bb0b95b7b0e54bdcc653e067
anniehuang <annie.huang921002@gmail.com>
2025-11-12 01:52:01 +0800

    add constroctor and interator.

---
+++ assignment6/src/convex_polygon.cpp
@@ -0,0 +1,86 @@
+#include "point.h"
+#include "convex_polygon.h"
+#include <cmath>
+#include <string>
+#include <vector>
+
+ConvexPolygon::ConvexPolygon() {}
+ConvexPolygon::ConvexPolygon(std::vector<Point *> vertices) : vertices_(vertices)
+{
+    for (Point *p : vertices)
+    {
+        if (p != nullptr)
+        {
+            this->vertices_.push_back(new Point(*p));
+        };
+        return;
+    }
+}
+ConvexPolygon::ConvexPolygon(const ConvexPolygon &other)
+{
+    for (const Point *other_point : other.vertices_)
+    {
+        this->vertices_.push_back(new Point(*other_point));
+    }
+}
+ConvexPolygon::~ConvexPolygon()
+{
+    for (Point *p : vertices_)
+    {
+        delete p;
+    }
+    vertices_.clear();
+}
+ConvexPolygon &ConvexPolygon::operator=(const ConvexPolygon &other)
+{
+    if (this != &other)
+    {
+        for (Point *p : vertices_)
+        {
+            delete p;
+        }
+        vertices_.clear();
+        for (const Point *other_point : other.vertices_)
+        {
+            this->vertices_.push_back(new Point(*other_point));
+        }
+    }
+    return *this;
+}
+double ConvexPolygon::area() const
+{
+    double area = 0.0;
+    int n = vertices_.size();
+    if (n < 3)
+    {
+        return 0.0;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        Point *current = vertices_[i];
+        Point *next = vertices_[(i + 1) % n];
+        area += (current->x * next->y) - (next->x * current->y);
+    }
+    return std::abs(area) / 2.0;
+}
+double ConvexPolygon::perimeter() const
+{
+    double perimeter = 0.0;
+    int n = vertices_.size();
+    if (n < 2)
+    {
+        return 0.0;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        Point *current = vertices_[i];
+        Point *next = vertices_[(i + 1) % n];
+        double length = distance(*current, *next);
+        perimeter += length;
+    }
+    return perimeter;
+}
+std::string ConvexPolygon::to_string() const
+{
+    return "ConvexPolygon";
+}
