b5d1892225926ef37c3fe2f258f3bd3c8926641d
anniehuang <annie.huang921002@gmail.com>
2025-12-07 20:55:58 +0800

    Update copy sh

--- copy.sh
+++ copy.sh
@@ -1,5 +1,5 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-echo $SCRIPT_DIR
+# echo $SCRIPT_DIR
 DATA_DIR="$SCRIPT_DIR/assignment7"
 echo $DATA_DIR
 TARGET_DIR="$SCRIPT_DIR/../oop2025f_111310452_hw"
