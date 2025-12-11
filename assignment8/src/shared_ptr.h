355831472fe207410cee07204cef489835af7dc5
anniehuang <annie.huang921002@gmail.com>
2025-12-12 00:08:12 +0800

    update test, add constructor and copy constructor

--- assignment8/src/shared_ptr.h
+++ assignment8/src/shared_ptr.h
@@ -2,26 +2,47 @@
 #define SHARED_PTR_H
 
 template <typename T>
-class SharedPtr{
+class SharedPtr
+{
 public:
     SharedPtr();
 
-    explicit SharedPtr(T* rawPtr);
+    explicit SharedPtr(T *rawPtr);
 
     SharedPtr(const SharedPtr<T> &other);
 
     ~SharedPtr();
 
-    SharedPtr<T>& operator=(const SharedPtr<T> &other);
+    SharedPtr<T> &operator=(const SharedPtr<T> &other);
 
-    T& operator*() const;
+    T &operator*() const;
 
     int reference_count() const;
 
     void reset();
 
 private:
-
+    T *_ptr;
+    int *_ref_count;
 };
+
+template <typename T>
+SharedPtr<T>::SharedPtr() : _ptr(nullptr), _ref_count(nullptr) {}
+template <typename T>
+SharedPtr<T>::SharedPtr(T *rawPtr) : _ptr(rawPtr), _ref_count(nullptr)
+{
+    if (_ptr != nullptr)
+    {
+        _ref_count = new int(1);
+    }
+}
+template <typename T>
+SharedPtr<T>::SharedPtr(const SharedPtr<T> &other) : _ptr(other._ptr), _ref_count(other._ref_count)
+{
+    if (_ref_count != nullptr)
+    {
+        *(_ref_count)++;
+    }
+}
 
 #endif // SHARED_PTR_H
--- assignment8/tests/ut_shared_ptr.cpp
+++ assignment8/tests/ut_shared_ptr.cpp
@@ -1,7 +1,12 @@
 #include <gtest/gtest.h>
+#include <memory>
 #include "shared_ptr.h"
 
 class SharedPtrTest : public ::testing::Test
 {
 protected:
 };
+
+TEST_F(SharedPtrTest, DefaultConstructor)
+{
+}
