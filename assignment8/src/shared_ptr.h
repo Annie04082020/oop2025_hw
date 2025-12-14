58f31a73ecbb1027822512f8c067a689de0db60f
anniehuang <annie.huang921002@gmail.com>
2025-12-14 20:39:11 +0800

    add operator, destructor, operator*, function reference_count, and functtion reset

--- assignment8/src/shared_ptr.h
+++ assignment8/src/shared_ptr.h
@@ -1,5 +1,6 @@
 #ifndef SHARED_PTR_H
 #define SHARED_PTR_H
+#include <stdexcept>
 
 template <typename T>
 class SharedPtr
@@ -44,5 +45,70 @@ SharedPtr<T>::SharedPtr(const SharedPtr<T> &other) : _ptr(other._ptr), _ref_coun
         *(_ref_count)++;
     }
 }
+template <typename T>
+SharedPtr<T> &SharedPtr<T>::operator=(const SharedPtr<T> &other)
+{
+    if (this == &other)
+    {
+        return *this;
+    }
+    this->reset();
+    this->_ptr = other._ptr;
+    this->_ref_count = other._ref_count;
+    if (other._ref_count != nullptr)
+    {
+        (*other._ref_count)++;
+    }
+    return *this;
+}
+template <typename T>
+SharedPtr<T>::~SharedPtr()
+{
+    if (_ptr == nullptr)
+    {
+        return;
+    }
+    (*_ref_count)--;
+    if (*_ref_count == 0)
+    {
+        delete _ptr;
+        delete _ref_count;
+    }
+}
+template <typename T>
+T &SharedPtr<T>::operator*() const
+{
+    if (this->_ptr == nullptr)
+    {
+        throw std::runtime_error("This pointer owns nothing.");
+    }
+    return *this->_ptr;
+}
+template <typename T>
+int SharedPtr<T>::reference_count() const
+{
+    if (this->_ref_count == nullptr)
+    {
+        return 0;
+    }
+    return *this->_ref_count;
+}
+template <typename T>
+void SharedPtr<T>::reset()
+{
+    if (_ptr == nullptr)
+    {
+        return;
+    }
 
+    (*_ref_count)--;
+
+    if (*_ref_count == 0)
+    {
+        delete _ptr;
+        delete _ref_count;
+    }
+    _ptr = nullptr;
+    _ref_count = nullptr;
+}
 #endif // SHARED_PTR_H
